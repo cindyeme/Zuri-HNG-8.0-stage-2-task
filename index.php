b8bfe3755b873fafeeba648de163faa90a73e969
cindyeme <emereninicynthia@gmail.com>
2021-07-08 09:30:45 +0100

    projects & experience update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
   <title>Emerenini Cynthia's Portfolio</title>
   <meta content="Portfolio" name="Emerenini Cynthia's portfolio">
-  <meta content="" name="keywords">
+  <meta content="emerenini-cynthia, cynthia, emerenini-cynthia-ngozi, ngozi, emerenini-ngozi" name="keywords">
 
   <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
@@ -38,8 +38,8 @@
   <header id="header" class="header-tops">
     <div class="container">
 
-      <h1><a href="index.html">Emerenini Cynthia Ngozi</a></h1>
-      <h2>I'm a passionate <span class="typed" data-typed-items="Web Designer, Developer, Freelancer, Web Content Writer"></span> from Nigeria</h2>
+      <h1><a href="index.php">Emerenini Cynthia Ngozi</a></h1>
+      <h2>I'm a passionate <span class="typed" data-typed-items="Frontend Developer, React Developer, UI Engineer"></span> from Nigeria</h2>
 
       <nav class="nav-menu  d-lg-block">
         <ul>
@@ -54,22 +54,27 @@
 
       <div class="social-links">
 
+        <!-- Github -->
         <a href="https://github.com/cindyeme" class="github" target="_blank"
         data-toggle="popover" data-trigger="hover" data-content="Github" data-placement="bottom">
           <i class="ri-github-fill"></i>
         </a>
+        <!-- Linkedin -->
         <a href="https://www.linkedin.com/in/emerenini-cynthia-ngozi" class="linkedin" target="_blank"
         data-toggle="popover" data-trigger="hover" data-content="Linkedin" data-placement="bottom">
           <i class="icofont-linkedin"></i>
         </a>
+        <!-- Medium -->
         <a href="https://medium.com/@emereninicynthia" class="medium"  target="_blank"
         data-toggle="popover" data-trigger="hover" data-content="Medium" data-placement="bottom">
           <i class="ri-medium-fill"></i>
         </a>
+        <!-- Twitter -->
         <a href="https://twitter.com/CynthiaENgozi1" class="twitter"  target="_blank"
         data-toggle="popover" data-trigger="hover" data-content="Twitter" data-placement="bottom">
           <i class="icofont-twitter"></i>
         </a>
+        <!-- Facebook -->
         <a href="https://web.facebook.com/cynthiaengozi" class="facebook"  target="_blank"
         data-toggle="popover" data-trigger="hover" data-content="Facebook" data-placement="bottom">
           <i class="icofont-facebook"></i>
@@ -96,19 +101,19 @@
           <img src="assets/img/face.png" class="img-fluid" alt="my_pic">
         </div>
         <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
-          <h3>Web Designer &amp; Web Content Writer</h3>
+          <h3>Front-End Developer</h3>
           <p class="font-italic">
-            I am Emerenini Cynthia Ngozi, a client-oriented, meticulous & innovative web designer with over one year of experience in developing contents, responsive web applications, cross browser compatible web pages and general delivery of high quality technology-oriented solutions. 
+            I am Emerenini Cynthia Ngozi, a design-oriented, detail-oriented, meticulous & innovative web designer passionate about combining beautiful code with beautiful design. I have experience developing and designing software for the web. 
           </p>
           <p class="font-italic pb-4">
-            Extremely passionate towards software architecture and design combined with sensitivity to serve the needs of individuals, small to medium-sized companies/organizations with the use of latest and proven technologies to create outstanding high performance and secure custom contents || websites. *I seek to obtain a creative and challenging position that utilizes knowledge of Web Development & Design. I intend to serve you better!!
+            Love learning new & better ways to create seemlesss user engaging interface with clean, efficient & scalable code. I seek to obtain a creative and challenging position that utilizes knowledge of Web Development & Design. I intend to serve you better!!
           </p>
           <div class="row">
             <div class="col-lg-6">
               <ul>
                 <li><i class="icofont-rounded-right"></i> <strong>Birthday:</strong> 14th March</li>
                 <li><i class="icofont-rounded-right"></i> <strong>Website:</strong> https://emereninicynthiangozi.herokuapp.com</li>
-                <li><i class="icofont-rounded-right"></i> <strong>Phone:</strong> +234 903 291 1321</li>
+                <li><i class="icofont-rounded-right"></i> <strong>Phone:</strong> (+234)903-2911-321</li>
                 <li><i class="icofont-rounded-right"></i> <strong>City:</strong> Owerri, Imo State, Nigeria</li>
               </ul>
             </div>
@@ -289,11 +294,11 @@
           <h3 class="resume-title">Summary</h3>
           <div class="resume-item pb-0">
             <h4>Emerenini Cynthia Ngozi</h4>
-            <p><em>Enthusiatic, Innovative and purpose-driven Web Designer &amp; Web Content Writer with 1+ years of experience eager to contribute to organizational growth and efficiency  through excellent organizational skills, attention to details, hard work and smart work.</em></p>
+            <p><em>Enthusiatic, Innovative and purpose-driven Front-End Designer eager to contribute to organizational growth and efficiency through excellent organizational skills, attention to details, excellent time management, team spirit and smart work.</em></p>
             <p>
             <ul>
               <li>Owerri City, Nigeria</li>
-              <li>+234 903-291-1321</li>
+              <li>(+234)903-2911-321</li>
               <li>emereninicynthia@gmail.com</li>
             </ul>
             </p>
@@ -324,34 +329,40 @@
         <div class="col-lg-6">
           <h3 class="resume-title">Professional Experience</h3>
           <div class="resume-item">
-            <h4>Front-End Development</h4>
-            <h5>2020</h5>
+            <h4>UI Engineer</h4>
+            <h5>2020 - Present</h5>
             <p><em class="border-bottom">Hireus.ng, Nigeria</em></p>
             <p>
             <ul>
-              <li>Developed visually appealing, functional and responsive web design.</li>
+              <li>
+                Architected and implemented the front-end of hireus.ng which lets users find & hire professionals of any field within the nation.
+              </li>
+              <li>
+                Write reusable code and libraries to a standard which makes it quick and easy to maintain in the future.
+              </li>
+              <li>
+                Ensure user experience determines design choices and as well develop features to enhance experience. 
+              </li>
+              <li>
+                Communicate and collaborate with multidisciplinary teams of engineers, designers and stakeholders.
+              </li>
             </ul>
+            
+          </div>
+          <div class="resume-item">
+            <h4>Front-End Designer</h4>
             <p><em class="border-bottom">Losintech.com, Nigeria</em></p>
             <p>
             <ul>
-              <li>Designed, implemented and monitored web pages, pluginsa and functionality for continous improvement</li>
-              <li>Oversaw troubleshooting and excellent resolution of bugs.</li>
-            </ul>
-            </p>
-          </div>
-          <div class="resume-item">
-            <h4>Web Content Writer</h4>
-            <h5>2019-2020</h5>
-            <p><em class="border-bottom">Bleautech.org, Nigeria</em></p>
-            <p>
-            <ul>
-              <li>Developed quality, creative and simplified web contents.</li>
-              <li>Delivers best contents for projects.</li>
-            </ul>
-            <p><em class="border-bottom">DSC IMSU, Nigeria</em></p>
-            <p>
-            <ul>
-              <li>Covered events in writings.</li>
+              <li>
+                Developed and shipped user friendly and highly interactive web applications.
+              </li>
+              <li>
+                Write modern, performant, and robust code for a diverse array of projects.
+              </li>
+              <li>
+                Tested sites in various browsers and devices to ensure cross-browser compatibility and mobile responsiveness.
+              </li>
             </ul>
             </p>
           </div>
@@ -368,18 +379,12 @@
               <li>Content Developer.</li>
             </ul>
             </p>
-            <p><em class="border-bottom">Losintech.com, Nigeria</em></p>
-            <p>
-            <ul>
-              <li>Front-End Developer.</li>
-            </ul>
-            </p>
           </div>
         </div>
         
         <div class="col-lg-12">
           <div class="d-flex justify-content-start cv">
-            <a href="assets/img/cv.jpg" class="my_cv" data-toggle="popover" data-content="Download CV" data-trigger="hover" data-placement="right" download>Download CV</a>
+            <a href="assets/img/Resume-Emerenini_Cynthia_Ngozi.pdf" class="my_cv" data-toggle="popover" data-content="Download CV" data-trigger="hover" data-placement="right">Download CV</a>
           </div>
         </div>
       </div>
@@ -400,10 +405,27 @@
       <div class="row portfolio-container mt-5">
 
         <div class="col-lg-4 col-md-6 portfolio-item">
+          <div class="portfolio-wrap">
+            <div class="portfolio-info">
+              <h4>HireUs</h4>
+              <p>A web application built to make finding & hiring of local professionals or experts of various fields faster and easier. You can either come to hire or get hired for your services!</p>
+              <p><strong>Technologies:</strong> React, TailwindCss</p>
+            </div>
+            <div class="portfolio-links d-flex justify-content-between">
+              <div class="logo">
+                <a href="#"><img src="assets/img/hireus.png" alt="hireus-logo" class="img-fluid"></a>
+              </div>
+              <div class="d-link position-relative">
+                <a href="http://cyhotel.herokuapp.com">View</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 portfolio-item">
           <div class="portfolio-wrap first-child">
             <div class="portfolio-info">
               <h4>Lib-Cater</h4>
-              <p>Lib-Cater manages all manual functions of a library. Allows the librarian to maintain library resources in a more operative manner that will help to save their time.</p>
+              <p>Lib-Cater is an independent project designed to manage all manual functions of a library. Allows the librarian to maintain library resources in a more operative manner that will help to save time.</p>
             </div>
             <div class="portfolio-links d-flex justify-content-between">
               <div class="logo">
@@ -418,41 +440,27 @@
         <div class="col-lg-4 col-md-6 portfolio-item">
           <div class="portfolio-wrap">
             <div class="portfolio-info">
-              <h4>Losintech</h4>
-              <p>(Frontend Design). A top rated custom software development company that designs and 
-              develops various sizes of application software.</p>
+              <h4>Tourism App</h4>
+              <p>A project with a custom software development company aimed at making tourism and exploration within any state in Nigeria a wonderful experience.</p>
             </div>
             <div class="portfolio-links d-flex justify-content-between">
               <div class="logo">
-                <a href="https://losintech.com"><img src="assets/img/losintech-lgo.png" alt="losintech-logo" class="img-fluid"></a>
+                <a href="https://imo-toursm.herokuapp.com">
+                  <img src="assets/img/losintech-lgo.png" alt="losintech-logo" class="img-fluid">
+                </a>
               </div>
-              <div class="img-box">
-                <a href="https://github.com/cindyeme/Losintech_website"><i class="ri-github-fill"></i></a>
+              <div class="d-link position-relative">
+                <a href="http://cyhotel.herokuapp.com">View</a>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-6 portfolio-item">
-          <div class="portfolio-wrap">
-            <div class="portfolio-info">
-              <h4>HireUs</h4>
-              <p>(Frontend Design). Hire over 100,000 experts in various fields.</p>
-            </div>
-            <div class="portfolio-links d-flex justify-content-between">
-              <div class="logo">
-                <a href="#"><img src="assets/img/hireus.png" alt="hireus-logo" class="img-fluid"></a>
-              </div>
-              <div class="img-box">
-                <a href="https://github.com/cindyeme/"><i class="ri-github-fill"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
+        
         <div class="col-lg-4 col-md-6 portfolio-item">
           <div class="portfolio-wrap">
             <div class="portfolio-info">
               <h4>CyHotel</h4>
-              <p>(Frontend Design). Hotel Booking Template. </p>
+              <p> A hotel template designed to tackle reservation tedious processes, contain other exclusive activities like Car, Gadgets renting, spa, kitchen etc </p>
             </div>
             <div class="portfolio-links d-flex justify-content-between">
               <div class="img-box">
@@ -464,6 +472,60 @@
             </div>
           </div>
         </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item">
+          <div class="portfolio-wrap">
+            <div class="portfolio-info">
+              <h4>Facebook</h4>
+              <p> A class project challenge to clone facebook web homepage. </p>
+            </div>
+            <div class="portfolio-links d-flex justify-content-between">
+              <div class="logo">
+                <a href="https://github.com/cindyeme/facebook-home-page">
+                  <img src="assets/img/fb-icon.jpg" alt="fb-logo" class="img-fluid">
+                </a>
+              </div>
+              <div class="d-link position-relative">
+                <a href="http://cyhotel.herokuapp.com">View</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item">
+          <div class="portfolio-wrap">
+            <div class="portfolio-info">
+              <h4>Ladies Empowerment</h4>
+              <p> An independent interface focused on bringing women together in order to help them discover their purpose. Encourages women in various fields to see themselves as ambassadors of a positive change in our society. </p>
+            </div>
+            <div class="portfolio-links d-flex justify-content-between">
+              <div class="img-box">
+                <a href="https://github.com/cindyeme/Woman-on-a-mission"><i class="ri-github-fill"></i></a>
+              </div>
+              <div class="d-link position-relative">
+                <a href="http://woman-mission.herokuapp.com">View</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item">
+          <div class="portfolio-wrap">
+            <div class="portfolio-info">
+              <h4>Udacity</h4>
+              <p> A class project challenge to clone udacity homepage. </p>
+            </div>
+            <div class="portfolio-links d-flex justify-content-between">
+              <div class="logo">
+                <a href="https://github.com/cindyeme/cloned-project"><img src="assets/img/udacity.png" alt="libcater-logo" class="img-fluid"></a>
+              </div>
+              <div class="img-box">
+                <a href="https://github.com/cindyeme/cloned-project"><i class="ri-github-fill"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div class="col-lg-4 col-md-6 portfolio-item">
           <div class="portfolio-wrap">
             <div class="portfolio-info">
@@ -577,9 +639,6 @@
             <img src="assets/img/gallery/gallery-17.PNG" class="img-fluid" alt="gallery_17">
           </a>
           <a href="assets/img/gallery/gallery-12.PNG" class="venobox" data-gall="gallery-carousel">
-            <img src="assets/img/gallery/gallery-18.PNG" class="img-fluid" alt="gallery_18">
-          </a>
-          <a href="assets/img/gallery/gallery-12.PNG" class="venobox" data-gall="gallery-carousel">
             <img src="assets/img/gallery/gallery-19.PNG" class="img-fluid" alt="gallery_19">
           </a>
         </div>
@@ -650,7 +709,8 @@
           <div class="info-box">
             <i class="bx bx-phone-call"></i>
             <h3>Call Me</h3>
-            <p>+234 903 291 1321</p>
+            <p>(+234)903-2911-321</p>
+            <p class="mt-2">(+234)0813-5570-186</p>
           </div>
         </div>
       </div>
